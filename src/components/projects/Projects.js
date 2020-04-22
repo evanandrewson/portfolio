cc348ad4c4a9d19176db3391cef5ced8893b6c43
evanandrewson <evanandrewson@gmail.com>
2020-04-22 14:24:50 -0700

    added crypto project to projects component

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,12 +2,25 @@ import React from 'react';
 import bashful from '../../assets/BASHful.png';
 import tone from '../../assets/tone.png';
 import tarot from '../../assets/tarot.png';
+import crypto from '../../assets/crypto.png';
 import styles from './Projects.css';
 
 
 const Projects = () => {
   return (
     <div className={styles.Projects}>
+      <section>
+        <img src={crypto} />
+        <div>
+          <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/Crypto-Punks/fe">Front-end Github</a></p>
+          <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/Crypto-Punks/be">Back-end Github</a></p>
+          <p><a target="_blank" rel="noopener noreferrer" href="https://crypto-trades-2020.netlify.app/">Deployed Site</a></p>
+          <p>
+            React, Redux, Node, Express, MongoDB, Mongoose < br/>
+            Fantasy cryptocurrency trading app with real time data <br />
+          </p>
+        </div>
+      </section>
       <section>
         <img src={bashful} />
         <div>
@@ -21,7 +34,8 @@ const Projects = () => {
       </section>
       <section>
         <div>
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/mood-mangoes/mood-mangoes.github.io">Github</a><a target="_blank" rel="noopener noreferrer" href="https://tone-check.herokuapp.com/">Deployed site</a>
+          <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/mood-mangoes/mood-mangoes.github.io">Github</a></p>
+          <p><a target="_blank" rel="noopener noreferrer" href="https://tone-check.herokuapp.com/">Deployed site</a></p>
           <p>
             Vanilla Javascript and SQL utilizing IBM Watson's API <br />
             Text sentiment analyzer <br />
@@ -33,7 +47,8 @@ const Projects = () => {
       <section>
         <img src={tarot} />
         <div>
-          <a target="_blank" rel="noopener noreferrer" href="https://github.com/tarot-reader/tarot-reader.github.io">Github</a><a target="_blank" rel="noopener noreferrer" href="https://tarot-reader.github.io/">Deployed site</a>
+          <p><a target="_blank" rel="noopener noreferrer" href="https://github.com/tarot-reader/tarot-reader.github.io">Github</a></p>
+          <p><a target="_blank" rel="noopener noreferrer" href="https://tarot-reader.github.io/">Deployed site</a></p>
           <p>
             Vanilla Javascript < br/>
             Randomized tarot readings and matching game <br />
